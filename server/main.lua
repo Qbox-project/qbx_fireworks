2cee3b2e9d80dbbd9802281d27565e532a7675d8
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-20 02:42:36 -0500

    refactor: placedFirework and useableitems

--- server/main.lua
+++ server/main.lua
@@ -1,41 +1,17 @@
--- Events
-RegisterNetEvent('qbx_fireworks:server:placedFirework', function(item)
-    local fireworkItems = {
-        'firework1',
-        'firework2',
-        'firework3',
-        'firework4',
-    }
+lib.versionCheck('Qbox-project/qbx_fireworks')
+
 local sharedConfig = require 'config.shared'
 
+RegisterNetEvent('qbx_fireworks:server:placedFirework', function(item)
     local player = exports.qbx_core:GetPlayer(source)
     if not player then return end
-    local foundItem = nil
-
-    for i = 1, #fireworkItems do
-        if fireworkItems[i] == item then
-            foundItem = fireworkItems[i]
-            break
-        end
-    end
-
-    if not foundItem then return end
-    player.Functions.RemoveItem(foundItem, 1)
+    
+    exports.ox_inventory:RemoveItem(source, item, 1)
 end)
 
--- Items
-exports.qbx_core:CreateUseableItem('firework1', function(source, item)
-    TriggerClientEvent('qbx_fireworks:client:usedFirework', source, item.name, 'proj_indep_firework')
-end)
-
-exports.qbx_core:CreateUseableItem('firework2', function(source, item)
-    TriggerClientEvent('qbx_fireworks:client:usedFirework', source, item.name, 'proj_indep_firework_v2')
-end)
-
-exports.qbx_core:CreateUseableItem('firework3', function(source, item)
-    TriggerClientEvent('qbx_fireworks:client:usedFirework', source, item.name, 'proj_xmas_firework')
-end)
-
-exports.qbx_core:CreateUseableItem('firework4', function(source, item)
-    TriggerClientEvent('qbx_fireworks:client:usedFirework', source, item.name, 'scr_indep_fireworks')
-end)
+for i = 1, #sharedConfig.fireworks do
+    local fireworks = sharedConfig.fireworks[i]
+    exports.qbx_core:CreateUseableItem(fireworks.itemName, function(source, item)
+        TriggerClientEvent('qbx_fireworks:client:usedFirework', source, item.name, fireworks.assetName)
+    end)
+end
