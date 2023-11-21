444a7376197aee3b889ce7fa5fff1ff92f909790
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-20 23:53:55 -0500

    fix: k, v names

--- server/main.lua
+++ server/main.lua
@@ -2,10 +2,10 @@ lib.versionCheck('Qbox-project/qbx_fireworks')
 
 local fireworks = require 'config.shared'.fireworks
 
-for k, v in pairs(fireworks) do
-    exports.qbx_core:CreateUseableItem(v.itemName, function(source)
-        local success = lib.callback.await('qbx_fireworks:client:useFirework', source, k)
+for asset, item in pairs(fireworks) do
+    exports.qbx_core:CreateUseableItem(item.itemName, function(source)
+        local success = lib.callback.await('qbx_fireworks:client:useFirework', source, asset)
         if not success then return end
-        exports.ox_inventory:RemoveItem(source, v.itemName, 1)
+        exports.ox_inventory:RemoveItem(source, item.itemName, 1)
     end)
 end
